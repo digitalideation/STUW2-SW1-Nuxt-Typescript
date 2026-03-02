25f3c5f2ff8d2963d5b041ce60230d355b9ff914
Nick Schneeberger <nick.schneeberger@icloud.com>
2026-03-02 09:49:10 +0100

    cleanup

--- app/pages/about.vue
+++
@@ -1,7 +0,0 @@
-<template>
-
-<h1>Ich bin die About Seite!</h1>
-
-<myButton></myButton>
-
-</template>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,26 +1,5 @@
 <template>
-
-<h1>Hello World!</h1>
-
-<nav>
-    <NuxtLink to="/about"> About </NuxtLink>
-</nav>
-
-<p>Ich bin die index Seite!</p>
-
-<myButton></myButton>
-
-<!-- Tailwind CSS v4 test -->
-<div class="mt-8 rounded-xl bg-blue-500 p-6 text-center text-white font-bold text-xl shadow-lg">
-  Tailwind CSS v4 is working!
-</div>
-
+  <h1 class="m-4 text-4xl">Hello World!</h1>
 </template>
 
-<style>
-
-h1 {
-    color: red;
-}
-
-</style>
+<style></style>
